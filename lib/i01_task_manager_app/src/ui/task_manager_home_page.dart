763fd201b1b216bdc613d537ee857c7e95b3bb10
Kitavrus <kitavrus@ya.ru>
2021-11-01 23:32:35 +0300

    TaskManagerHomePage start

--- lib/i01_task_manager_app/src/ui/task_manager_home_page.dart
+++ lib/i01_task_manager_app/src/ui/task_manager_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:coolicons/coolicons.dart';
 import 'package:flutter/material.dart';
 
 class TaskManagerHomePage extends StatelessWidget {
@@ -5,9 +6,74 @@ class TaskManagerHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('OK'),
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Positioned(
+              left: 16,
+              bottom: 0,
+              right: 16,
+              top: 16,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const CircleAvatar(
+                        backgroundColor: Colors.black,
+                        foregroundColor: Colors.white,
+                        child: Icon(Icons.apps),
+                      ),
+                      // Icon(Icons.apps),
+                      const Text(
+                        'Task Manager',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Stack(
+                        children: const [
+                          Icon(Coolicons.notification_outline),
+                          Positioned(
+                            right: 0,
+                            top: 0,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.red,
+                              radius: 4,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                height: 120,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Colors.white.withOpacity(0.70),
+                      Colors.white.withOpacity(0.75),
+                      Colors.white.withOpacity(0.50),
+                      Colors.white.withOpacity(0.25),
+                      Colors.white.withOpacity(0.1),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
