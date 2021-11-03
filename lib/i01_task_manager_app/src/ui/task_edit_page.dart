6e4b261bf15040e3da02b1d505ebdb4fd2f6f2af
Kitavrus <kitavrus@ya.ru>
2021-11-03 23:51:27 +0300

    start edit task page

--- lib/i01_task_manager_app/src/ui/task_edit_page.dart
+++ lib/i01_task_manager_app/src/ui/task_edit_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TaskEditPage extends StatelessWidget {
@@ -6,8 +7,44 @@ class TaskEditPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        color: Colors.red,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text("Edit Task"),
+        backgroundColor: Colors.white,
+        foregroundColor:
+            Colors.black, // TODO можно так задать цвет кнопки назад
+        elevation: 0,
+        // iconTheme: IconTheme.of(context).copyWith(color: Colors.black), // TODO можно так задать цвет кнопки назад
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Expanded(
+              child: Placeholder(),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(12)),
+                child: const Center(
+                  child: Text(
+                    'Save Task',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
