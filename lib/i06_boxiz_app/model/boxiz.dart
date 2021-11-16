f2a07ac4ec2811a3f72b1dc9c94a7bddfebf1fab
Kitavrus <kitavrus@ya.ru>
2021-11-16 13:44:05 +0300

    card item

---
+++ lib/i06_boxiz_app/model/boxiz.dart
@@ -0,0 +1,50 @@
+class BoxizGrid {
+  bool isAvailable;
+  int? number;
+  bool? isSelect;
+
+  BoxizGrid(this.number, this.isSelect, {this.isAvailable = true});
+}
+
+class BoxizSelect {
+  int? number;
+  bool isOpen;
+
+  BoxizSelect({this.number, required this.isOpen});
+}
+
+class Boxiz {
+  String? img;
+  String? prize;
+  String? type;
+  String? title;
+  String? datetime;
+  String? coin;
+  List<String>? items;
+  Duration? liveIn;
+
+  Boxiz({this.img, this.prize, this.type, this.title, this.datetime, this.coin, this.items, this.liveIn});
+}
+
+List<Boxiz> boxizItems = [
+  Boxiz(
+    title: "Bulls @ Lakers",
+    datetime: "Jun 20 @ 8:30PM ET",
+    coin: "50 Box Coin",
+    liveIn: Duration(seconds: 54, minutes: 4),
+    prize: "VIRTUAL PRIZE",
+    type: "NBA",
+    items: ["05", "17", "35", "68", "92"],
+    img: "https://cdn.pixabay.com/photo/2013/03/21/15/52/basketball-95607_960_720.jpg",
+  ),
+  Boxiz(
+    title: "Bulls @ Lakers",
+    datetime: "Jun 20 @ 8:30PM ET",
+    coin: "50 Box Coin",
+    liveIn: Duration(seconds: 54, minutes: 4),
+    prize: "VIRTUAL PRIZE",
+    type: "NBA",
+    items: ["05", "17", "35", "68", "92"],
+    img: "https://cdn.pixabay.com/photo/2013/03/21/15/52/basketball-95607_960_720.jpg",
+  )
+];
--- lib/i06_boxiz_app/ui/boxiz_home_page.dart
+++ lib/i06_boxiz_app/ui/boxiz_home_page.dart
@@ -1,5 +1,6 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_layout1/i06_boxiz_app/model/boxiz.dart';
 
 class BoxizHomePage extends StatelessWidget {
   const BoxizHomePage({Key? key}) : super(key: key);
@@ -10,65 +11,202 @@ class BoxizHomePage extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
+            const TopAppBar(),
             Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const CircleAvatar(
-                        radius: 12,
-                      ),
-                      Icon(Icons.comment_bank_outlined),
-                      Text('128'),
-                      Icon(Icons.vpn_key),
-                      Text('128'),
-                      Icon(Icons.shield),
-                      Text('3'),
-                      Badge(
-                        child: Icon(
-                          Icons.notifications,
-                          color: Colors.white,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                flex: 2),
-            const Expanded(child: Placeholder(), flex: 20),
-            Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.home),
-                          iconSize: 28),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.radio_button_checked),
-                          iconSize: 28),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.shield),
-                          iconSize: 28),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.waterfall_chart),
-                          iconSize: 28),
-                      IconButton(
-                          onPressed: () {},
-                          icon: Icon(Icons.confirmation_num_outlined),
-                          iconSize: 28),
-                    ],
-                  ),
-                ),
-                flex: 2),
+              child: ListView.builder(
+                  itemCount: boxizItems.length,
+                  itemBuilder: (context, index) {
+                    Boxiz _boxiz = boxizItems[index];
+                    return CardItemMy(boxiz: _boxiz);
+                  }),
+              flex: 20,
+            ),
+            const BottomNavBar(),
           ],
         ),
       ),
     );
+  }
+}
+
+class CardItemMy extends StatelessWidget {
+  const CardItemMy({
+    required Boxiz boxiz,
+    Key? key,
+  })  : _boxiz = boxiz,
+        super(key: key);
+
+  final Boxiz _boxiz;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: 380,
+        child: Container(
+          decoration: const BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(16),
+              topLeft: Radius.circular(16),
+            ),
+            // image: DecorationImage(
+            //   image: NetworkImage(_boxiz.img ?? ''),
+            //   fit: BoxFit.contain,
+            // ),
+          ),
+          child: Column(
+            children: [
+              Image.network(
+                _boxiz.img ?? '',
+                height: MediaQuery.of(context).size.height * 0.3,
+                fit: BoxFit.fill,
+                alignment: Alignment.topCenter,
+              ),
+              // Expanded(
+              //   flex: 5,
+              //   child: SizedBox.shrink(),
+              // ),
+              // Expanded(
+              //   flex: 4,
+              //   child: Column(
+              //     children: [
+              //       Row(
+              //         children: [
+              //           Text('data'),
+              //         ],
+              //       )
+              //     ],
+              //   ),
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CardItem extends StatelessWidget {
+  const CardItem({
+    required Boxiz boxiz,
+    Key? key,
+  })  : _boxiz = boxiz,
+        super(key: key);
+
+  final Boxiz _boxiz;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: 380,
+        child: Column(
+          children: [
+            Expanded(
+              flex: 5,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: const BorderRadius.only(
+                    topRight: Radius.circular(16),
+                    topLeft: Radius.circular(16),
+                  ),
+                  image: DecorationImage(
+                    image: NetworkImage(_boxiz.img ?? ''),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text('data'),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TopAppBar extends StatelessWidget {
+  const TopAppBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const CircleAvatar(
+                radius: 12,
+              ),
+              Icon(Icons.comment_bank_outlined),
+              Text('128'),
+              Icon(Icons.vpn_key),
+              Text('128'),
+              Icon(Icons.shield),
+              Text('3'),
+              Badge(
+                child: Icon(
+                  Icons.notifications,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+        ),
+        flex: 2);
+  }
+}
+
+class BottomNavBar extends StatelessWidget {
+  const BottomNavBar({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                  onPressed: () {}, icon: Icon(Icons.home), iconSize: 28),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.radio_button_checked),
+                  iconSize: 28),
+              IconButton(
+                  onPressed: () {}, icon: Icon(Icons.shield), iconSize: 28),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.waterfall_chart),
+                  iconSize: 28),
+              IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.confirmation_num_outlined),
+                  iconSize: 28),
+            ],
+          ),
+        ),
+        flex: 2);
   }
 }
